ae106dab1d1cb5d828220eec6902c5a152c232a0
Sebastien Fabbro <sebfabbro@gmail.com>
2010-08-31 10:58:05 -0700

    Moved around canonical_host

--- cfitsio-repack.bash
+++ cfitsio-repack.bash
@@ -5,7 +5,7 @@ wget ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio${version/./}.tar.gz
 tar xf cfitsio${version/./}.tar.gz
 cd cfitsio
 wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/configure.ac
-wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/Makefile.a
-wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/ax_pthreads.m4
+wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/Makefile.am
+wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/ax_pthread.m4
 sed -i -e "s/@VERSION@/${version}/" configure.ac
 autoreconf -fvi
