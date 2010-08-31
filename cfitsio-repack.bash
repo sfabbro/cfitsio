e19cfdfefd05d3974254d15f0622512efffeab14
Sebastien Fabbro <sebfabbro@gmail.com>
2010-08-31 11:42:33 -0700

    Now get threads from git repo on savannah

--- cfitsio-repack.bash
+++ cfitsio-repack.bash
@@ -8,7 +8,7 @@ tar xf cfitsio${version/./}.tar.gz
 cd cfitsio
 wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/configure.ac
 wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/Makefile.am
-wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/ax_pthread.m4
+wget "http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_pthread.m4" -O ax_pthread.m4
 sed -i -e "s/@VERSION@/${version}/" configure.ac
 rm -f Makefile.in cfitsio.in
 autoreconf -fi 
