946630421d0e7fa10573a4b3f615b8834a8ec454
Sebastien Fabbro <sebfabbro@gmail.com>
2010-08-31 17:09:17 -0700

    Lots of changes for globus, fortran and threads checking. Now use ax_pthread.m4 from savannah

--- cfitsio-repack.bash
+++ cfitsio-repack.bash
@@ -5,13 +5,14 @@ echo " >>> Repacking cfitsio version ${version}"
 echo
 wget ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio${version/./}.tar.gz
 tar xf cfitsio${version/./}.tar.gz
-cd cfitsio
+pushd cfitsio > /dev/null
 wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/configure.ac
 wget http://gitorious.org/poloka/cfitsio/blobs/raw/master/Makefile.am
 wget "http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_pthread.m4" -O ax_pthread.m4
 sed -i -e "s/@VERSION@/${version}/" configure.ac
-rm -f Makefile.in cfitsio.in
+rm -f Makefile.in configure.in
 autoreconf -fi 
+popd > /dev/null
 echo
-echo " >>> Now run ./configure with your options (--help is your friend)"
-
+echo " >>> Now run ./cfitsio/configure --help to see your compile options"
+echo
