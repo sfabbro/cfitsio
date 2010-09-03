6a54060e1a642781d963c6987262df37e469cfac
Sebastien Fabbro <sebfabbro@gmail.com>
2010-09-03 10:31:29 -0700

    Split the repack script into two simple ones

---
+++ make-cfitsio-tarball.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+VERSION=${1:-3.250}
+
+UT="cfitsio${VERSION/./}.tar.gz"
+echo -n " >>> Fetching ${UT} ... "
+wget -q -c "ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/${UT}"
+[ ! -e "${UT}" ] && echo "\n   error fetching upstream tar ball" && exit
+tar xfz ${UT}
+echo "OK"
+
+echo -n " >>> Fetching patches ... "
+pushd cfitsio > /dev/null
+wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/configure.ac
+wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/Makefile.am
+wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/ax_pthread.m4
+[ ! -e configure.ac -o ! -e Makefile.am -o ! -e ax_pthread.m4 ] && echo "\n   error fetching patches" && exit
+sed -i -e "s/@VERSION@/${version}/" configure.ac
+echo "OK"
+
+echo -n " >>> Producing new tar ball ... "
+rm -f Makefile.in configure.in
+autoreconf -fi &> /dev/null
+./configure -q > /dev/null
+make -s dist
+P="cfitsio-${VERSION}"
+[ ! -e "${P}.tar.gz" ] && echo "\n   error producing tar ball" && exit
+echo "OK"
+
+mv ${P}.tar.gz ../
+popd > /dev/null
+rm -rf cfitsio/ ${UT}
+echo " >>> ${P}.tar.gz is ready"
