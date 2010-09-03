b75c28c767dc4ba65f58ca3be813a1a8eff11d04
Sebastien Fabbro <sebfabbro@gmail.com>
2010-09-02 17:37:50 -0700

    removing badly put comment

--- cfitsio-repack.bash
+++ cfitsio-repack.bash
@@ -15,14 +15,13 @@ P="cfitsio-${VERSION}"
 NP="cfitsio${VERSION/./}"
 
 echo " >>> Fetching ${NP}..."
-#wget -q -c ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/${NP}.tar.gz
+wget -q -c ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/${NP}.tar.gz
 tar xfz ${NP}.tar.gz
 echo " >>> Repacking cfitsio..."
 pushd cfitsio > /dev/null
-#wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/configure.ac
-#wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/Makefile.am
-#wget -q "http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_pthread.m4" -O ax_pthread.m4
-cp ~/src/cfitsio/* .
+wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/configure.ac
+wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/Makefile.am
+wget -q "http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_pthread.m4" -O ax_pthread.m4
 sed -i -e "s/@VERSION@/${version}/" configure.ac
 rm -f Makefile.in configure.in
 autoreconf -fi &> /dev/null
