c567d65a37181d79428e9a39298ace4678ad21fa
Sébastien Fabbro <sebfabbro@gmail.com>
2011-01-23 21:13:00 +0000

    Updated all scripts for cfitsio-3.26. Now uses autoheader

--- make-cfitsio-tarball.sh
+++ make-cfitsio-tarball.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
 
-VERSION=${1:-3.250}
+VERSION=${1:-3.260}
 
 UT="cfitsio${VERSION/./}.tar.gz"
-echo -n " >>> Fetching ${UT} ... "
-wget -q -c "ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/${UT}"
-[ ! -e "${UT}" ] && echo "\n   error fetching upstream tar ball" && exit
+echo " >>> Fetching ${UT} ..."
+#wget -q -c "ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/${UT}"
+[ ! -e "${UT}" ] && echo "   error fetching upstream ${UT}" && exit
 tar xfz ${UT}
-echo "OK"
 
-echo -n " >>> Fetching patches ... "
+echo " >>> Fetching patches ..."
 pushd cfitsio > /dev/null
-wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/configure.ac
-wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/Makefile.am
-wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/ax_pthread.m4
-[ ! -e configure.ac -o ! -e Makefile.am -o ! -e ax_pthread.m4 ] && echo "\n   error fetching patches" && exit
+for f in configure.ac Makefile.am ax_pthread.m4 ax_check_zlib.m4 cfitsio-autoheader.patch; do
+    #wget -q http://gitorious.org/poloka/cfitsio/blobs/raw/master/${f}
+    cp ../${f} .
+    [ ! -e  ${f} ] && echo "   error fetching patch $f" && exit
+done
+patch -sp0 < cfitsio-autoheader.patch
 sed -i -e "s/@VERSION@/${version}/" configure.ac
-echo "OK"
-
-echo -n " >>> Producing new tar ball ... "
+echo " >>> Producing new tar ball ..."
 rm -f Makefile.in configure.in
 autoreconf -i &> /dev/null
 ./configure -q > /dev/null
-make -s dist
+make -s distcheck
 P="cfitsio-${VERSION}"
-[ ! -e "${P}.tar.gz" ] && echo "\n   error producing tar ball" && exit
-echo "OK"
+[ ! -e "${P}.tar.gz" ] && echo "   error producing tar ball" && exit
 
 mv ${P}.tar.gz ../
 popd > /dev/null
-rm -rf cfitsio/ ${UT}
+#rm -rf cfitsio/ ${UT}
 echo " >>> ${P}.tar.gz is ready"
