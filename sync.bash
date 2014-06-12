8c08b6ef723df8e1bfef8c1b61371c76d7d9ebcc
Sebastien Fabbro <sfabbro@uvic.ca>
2014-06-12 09:36:26 -0700

    build.bash

--- sync.bash
+++ sync.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-[ $# -lt 1 ] && echo "$0 <cfitsio version>" && exit
-
-PV=${1}
-
 # sync cfitsio upstream
-wget -nc -c ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio${PV/./}.tar.gz
-ln -sfn cfitsio${PV/./}.tar.gz cfitsio.tar.gz 
+rm -f cfitsio_latest.tar.gz
+wget -c ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio_latest.tar.gz
 
 # sync m4 macro
 wget http://git.savannah.gnu.org/cgit/autoconf-archive.git/plain/m4/ax_pthread.m4 \
-    -O addons/m4/ax_pthread.m4 
+    -O autotools/m4/ax_pthread.m4 
 
+tar xf cfitsio_latest.tar.gz
+
+PV="$(awk -F , '/CFITSIO_MAJOR/{print $2}' cfitsio/configure.in | sed -e 's|)||')"
+PV="${PV}.$(awk -F , '/CFITSIO_MINOR/{print $2}' cfitsio/configure.in | sed -e 's|)||')"
 # update version number
-sed -i -e "/AC_INIT/s/[[:digit:]]\.[[:digit:]]\{3\}/${PV}/" addons/configure.ac
+sed -i -e "/AC_INIT/s/[[:digit:]]\.[[:digit:]]\{3\}/${PV}/" autotools/configure.ac
