fa1fa42a366b0a6f4d145d51250cffe0062871d1
Sébastien Fabbro <sebfabbro@gmail.com>
2016-06-06 14:19:44 -0700

    versioning done in build.bash

--- sync.bash
+++ sync.bash
@@ -10,8 +10,3 @@ for macro in prog_flex prog_bison; do
     wget http://git.savannah.gnu.org/cgit/autoconf-archive.git/plain/m4/ax_${macro}.m4 \
 	-O autotools/m4/ax_${macro}.m4 
 done
-
-# update version number and add a zero to fit upstream versioning
-PV="$(awk -F , '/CFITSIO_MAJOR/{print $2}' cfitsio/configure.in | sed -e 's|)||')"
-PV="${PV}.$(awk -F , '/CFITSIO_MINOR/{print $2}' cfitsio/configure.in | sed -e 's|)||')"
-sed -i -e "/AC_INIT/s/[[:digit:]]\.[[:digit:]]\{3\}/${PV}/" autotools/configure.ac
